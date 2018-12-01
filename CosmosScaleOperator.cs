94171bd9f9f0d4e1331a9356fb3c5730adddaa05
Gio <tediashvili.giorgi@gmail.com>
2018-12-01 14:17:43 -0600

    insert works

--- CosmosScaleOperator.cs
+++ CosmosScaleOperator.cs
@@ -1,5 +1,8 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
 using Microsoft.Azure.Documents.Client;
 
 namespace CosmosScale
@@ -8,25 +11,77 @@ namespace CosmosScale
     {
         private DocumentClient _client;
         private int _minRu, _maxRu;
+        private Uri _collectionUri;
         private string _databaseName, _collectionName;
+        private string _collectionPartitionKey;
+        private const int _maximumRetryCount = 30;
 
-        public CosmosScaleOperator(int minimumRu, int maximumRu, string databaseName, string collectionName, DocumentClient client)
+        public CosmosScaleOperator(int minimumRu, int maximumRu, string databaseName, string collectionName, DocumentClient client, string partitionKeyPropertyName = "/id")
         {
             _client = client;
             _minRu = minimumRu;
             _maxRu = maximumRu;
             _databaseName = databaseName;
             _collectionName = collectionName;
+            _collectionPartitionKey = partitionKeyPropertyName;
+            _collectionUri = UriFactory.CreateDocumentCollectionUri(databaseName, collectionName);
         }
-        public void GetDocument()
+
+        public IEnumerable<T> QueryCosmos<T>(string cosmosSqlQuery, int maxItemCount = -1, bool QueryCrossPartition = true)
         {
+            FeedOptions queryOptions = new FeedOptions { MaxItemCount = maxItemCount, EnableCrossPartitionQuery = QueryCrossPartition };
 
+            return _client.CreateDocumentQuery<T>(
+                    _collectionUri,
+                    new SqlQuerySpec()
+                    {
+                        QueryText = cosmosSqlQuery
+                    },
+                    queryOptions);
         }
-        public void InsertDocument()
+
+        #region INSERT
+        public async Task<CosmosOperationResponse> InsertDocument(object document)
         {
+            CosmosOperationResponse result = new CosmosOperationResponse();
+            return await InsertDocument(document, 0, result);
+        }
+        private async Task<CosmosOperationResponse> InsertDocument(object document, int retryCount, CosmosOperationResponse result)
+        {
+            try
+            {
+                await _client.CreateDocumentAsync(_collectionUri, document);
+                result.Success = true;
+                result.TotalRetries = retryCount;
+                return result;
+            }
+            catch (Exception e)
+            {
+                if (e.Message.Contains("Request rate is large"))
+                {
+                    if (retryCount > _maximumRetryCount)
+                    {
+                        result.Success = false;
+                        result.TotalRetries = retryCount;
+                        return result;
+                    }
+                    else
+                    {
+                        var op = await ScaleLogic.ScaleUpCollection(_client, _databaseName, _collectionName, _minRu);
+                        result.ScaleOperations.Add(op);
+                        return await InsertDocument(document, retryCount++, result);
+                    }
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
         }
+        #endregion
 
+        #region DELETE
         public void DeleteDocument()
         {
 
@@ -36,6 +91,24 @@ namespace CosmosScale
         {
 
         }
+        #endregion
+       
 
+    }
+
+    public class CosmosOperationResponse
+    {
+        public bool Success { get; set; }
+        public List<ScaleOperation> ScaleOperations { get; set; } = new List<ScaleOperation>();
+        public int TotalRetries { get; set; }
+    }
+
+    public class ScaleOperation
+    {
+        public int ScaledFrom { get; set; }
+        public int ScaledTo { get; set; }
+        public DateTimeOffset OperationTime { get; set; }
+        public bool ScaledSuccess { get; set; } = true;
+        public string ScaleFailReason { get; set; }
     }
 }
--- ScaleLogic.cs
+++ ScaleLogic.cs
@@ -1,18 +1,80 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace CosmosScale
 {
-    public class ScaleLogic
+    public static class ScaleLogic
     {
-        public void ScaleUpCollection()
+        private static DateTimeOffset latestScaleUp = DateTime.MinValue;
+        private static int currentRu = -1;
+
+        public static async Task<ScaleOperation> ScaleUpCollection(DocumentClient _client, string _databaseName, string _collectionName, int minRu)
         {
+            try
+            {
+                if (DateTime.Now.AddSeconds(-5) < latestScaleUp)
+                {
+                    return new ScaleOperation()
+                    {
+                        ScaledSuccess = false,
+                        ScaleFailReason = "There has been another scale within 5 seconds."
+                    };
+                }
 
+                Database database = _client.CreateDatabaseQuery($"SELECT * FROM d WHERE d.id = \"{_databaseName}\"").AsEnumerable().First();
+
+                List<DocumentCollection> collections = _client.CreateDocumentCollectionQuery((String)database.SelfLink).ToList();
+
+                foreach (var collection in collections)
+                {
+                    if (collection.Id == _collectionName)
+                    {
+                        Offer offer = _client.CreateOfferQuery()
+                            .Where(r => r.ResourceLink == collection.SelfLink)
+                            .AsEnumerable()
+                            .SingleOrDefault();
+
+                        if (currentRu == -1)
+                        {
+                            currentRu = minRu;
+                        }
+
+                        offer = new OfferV2(offer, (int)currentRu + 500);
+
+                        await _client.ReplaceOfferAsync(offer);
+
+                        latestScaleUp = DateTime.Now;
+                        currentRu = currentRu + 500;
+
+                        ScaleOperation op = new ScaleOperation();
+                        op.ScaledFrom = (int)currentRu;
+                        op.ScaledTo = (int)currentRu + 500;
+                        op.OperationTime = DateTime.Now;
+                        return op;
+                    }
+                }
+
+                return new ScaleOperation()
+                {
+                    ScaledSuccess = false,
+                    ScaleFailReason = "Could not find the collection to scale."
+                };
+            } catch (Exception e)
+            {
+                return new ScaleOperation()
+                {
+                    ScaledSuccess = false,
+                    ScaleFailReason = e.Message
+                };
+            }
+           
         }
-
-        public void ScaleDownCollection()
-        {
-
-        }
-
     }
+
+
 }
