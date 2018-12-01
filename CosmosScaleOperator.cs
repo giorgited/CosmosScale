3b3c544fd1011285b79b8c4d67ab73de804e5aef
Giorgi Tediashvili <gxt150530@utdallas.edu>
2018-12-01 09:46:13 -0600

    initial commit

---
+++ CosmosScaleOperator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Microsoft.Azure.Documents.Client;
+
+namespace CosmosScale
+{
+    public class CosmosScaleOperator
+    {
+        private DocumentClient _client;
+        private int _minRu, _maxRu;
+        private string _databaseName, _collectionName;
+
+        public CosmosScaleOperator(int minimumRu, int maximumRu, string databaseName, string collectionName, DocumentClient client)
+        {
+            _client = client;
+            _minRu = minimumRu;
+            _maxRu = maximumRu;
+            _databaseName = databaseName;
+            _collectionName = collectionName;
+        }
+        public void GetDocument()
+        {
+
+        }
+        public void InsertDocument()
+        {
+
+        }
+
+        public void DeleteDocument()
+        {
+
+        }
+
+        public void UpdateDocument()
+        {
+
+        }
+
+    }
+}
---
+++ ScaleLogic.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace CosmosScale
+{
+    public class ScaleLogic
+    {
+        public void ScaleUpCollection()
+        {
+
+        }
+
+        public void ScaleDownCollection()
+        {
+
+        }
+
+    }
+}
